533fe95a7b144d41c53f96d7df9d9bfc62699d2a
Rolando Panez <rolandop@checkpoint.com>
2020-10-06 23:27:29 +0000

    reorganizing code

--- modules/cgcspm/output.tf
+++
@@ -1,3 +0,0 @@
-output "clusterID" {
-	value = dome9_cloudaccount_kubernetes.demo.id
-}
--- modules/cgcspm/versions.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  required_providers {
-    dome9 = {
-      source = "terraform-providers/dome9"
-    }
-  }
-  required_version = ">= 0.13"
-}
